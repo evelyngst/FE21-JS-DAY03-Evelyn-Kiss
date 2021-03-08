ee1b0a3d98848ee23384305d87a2512aaab76b5d
Erich Kiss <erich.kiss2@gmail.com>
2021-03-08 11:56:05 +0100

    trial and error

--- basic_exercise_3/script.js
+++ basic_exercise_3/script.js
@@ -5,24 +5,40 @@
 // } else {
 //     document.getElementById("temp").innerHTML = "The weather is moderate";
 // }
-var degrees = Math.floor(Math.random() * 30 ) -5;
-console.log(degrees);
 
-switch(degrees) {
-    case degrees <= 10 :
-        title = 'The weather is cold';
-        console.log(title);
-        break;
-    case 10 < degrees <= 20:
-        title = 'The weather is moderate';
-        console.log(title);
-        break;
-    case  20 < degrees <= 30:
-        title = 'The weather is nice';
-        console.log(title);
-        break;
-    case 30 < degrees:
-        title = 'The weather is hot';
-        console.log(title);
-        break; 
-}
+
+// var degrees = Math.floor(Math.random() * 30 ) -5;
+// console.log(degrees + " the current degrees");
+// console.log(title + " the initial value");
+
+
+
+// switch(degrees) {
+//     case 1:
+//         console.log(degrees + " cold");
+//         break;
+//     case 2:
+//         console.log(degrees + " moderate");
+//         break;
+//     case 3:    
+//         console.log(degress + " hot");
+//         break;
+// }
+
+    // case degrees <= 10:
+    //     title = 'The weather is cold';
+    //     console.log(title + " cold");
+    //     break;
+    // case degrees > 10 && degrees <= 20:
+    //     title = 'The weather is moderate';
+    //     console.log(title + " moderate");
+    //     break;
+    // case degrees > 20 && degrees <= 30:
+    //     title = 'The weather is nice';
+    //     console.log(title + " nice");
+    //     break;
+    // case degrees > 30:
+    //     title = 'The weather is hot';
+    //     console.log(title + " hot");
+    //     break; 
+// }
