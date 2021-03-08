c95221c58ba3cd0a26c194476ef58767894e4de4
Erich Kiss <erich.kiss2@gmail.com>
2021-03-08 14:24:39 +0100

    without class average

--- Intermediate_exercise_1/script.js
+++ Intermediate_exercise_1/script.js
@@ -8,38 +8,44 @@ var studentName = ["Martin", "Thomas", "Klaus", "Maria", "David"];
 var studentGrade = ["76", "85", "65", "93", "81"];
 var output = "";
 
-for (let i of studentName) {
-    output += "The students name is " + i + "<br>";
+for (let i in studentName) {
+    output += "The students name is " + studentName[i] + "<br>";
+    output += "The average is " +studentGrade[i] + "<br>";
+    
+
+
+    if (studentGrade[i] < 60) {
+        output += "The grade is F <br> <br>";
+        // document.getElementById("outputGrade").innerHTML += "The grade is F <br>";
+        // document.getElementById("cold").style.display = "block";
+    } else {
+        if (studentGrade[i] < 70) {
+            output += "The grade is D <br> <br>";
+            // document.getElementById("outputGrade").innerHTML = "The grade is D <br>";
+            // document.getElementById("moderate").style.display = "block";
+        } else {
+            if (studentGrade[i] < 80) {
+                output += "The grade is C <br> <br>";
+                // document.getElementById("outputGrade").innerHTML = "The grade is C <br>";
+                // document.getElementById("nice").style.display = "block";
+            } else {
+                if (studentGrade[i] < 90) {
+                    output += "The grade is B <br> <br>";
+                    // document.getElementById("outputGrade").innerHTML = "The grade is B <br>";
+                    // document.getElementById("nice").style.display = "block";
+                }
+                else {
+                    output += "The grade is A <br> <br>";
+
+                    // degrees > 30;
+                    // document.getElementById("outputGrade").innerHTML = "The grade is A <br>";
+                    // document.getElementById("hot").style.display = "block";
+                }
+            }
+        }
+
+    }
     document.getElementById("outputGrade").innerHTML = output;
-
-
-    // if (studentGrade < 60) {
-    //     document.getElementById("outputGrade").innerHTML += "The grade is F <br>";
-    //     // document.getElementById("cold").style.display = "block";
-    // } else {
-    //     if (studentGrade < 70) {
-    //         document.getElementById("outputGrade").innerHTML = "The grade is D <br>";
-    //         // document.getElementById("moderate").style.display = "block";
-    //     } else {
-    //         if (studentGrade < 80) {
-    //             document.getElementById("outputGrade").innerHTML = "The grade is C <br>";
-    //             // document.getElementById("nice").style.display = "block";
-    //         } else {
-    //             if (studentGrade < 90) {
-    //                 document.getElementById("outputGrade").innerHTML = "The grade is B <br>";
-    //                 // document.getElementById("nice").style.display = "block";
-    //             }
-    //             else {
-
-
-    //                 // degrees > 30;
-    //                 document.getElementById("outputGrade").innerHTML = "The grade is A <br>";
-    //                 // document.getElementById("hot").style.display = "block";
-    //             }
-    //         }
-    //     }
-
-    // }
 
 }
 
